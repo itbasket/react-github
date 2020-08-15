2ffef52a4863981a689e33cbd2e4c16b337f3470
Vladislav Podkaura <tripledoublehs@ukr.net>
2020-08-15 13:31:14 +0300

    Navigation added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Navbar } from './components/Navbar'
+import { Home } from './pages/Home'
 
 function App() {
   return (
-    <div className='container pt-4'>
-      Hello World
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <div className='container pt-4'>
+        <Home />
+      </div>
+    </React.Fragment>
   )
 }
 
